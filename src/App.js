c6e13147caa567dc34250ddf073ddf86c57e0687
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-09-19 07:55:58 -0600

    Component test and import in to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
 import './App.css';
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Main/>
      <h2>CO Healthy Homemade</h2>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = (props) => {
+    return <h1>Header: Test</h1>
+  }
+  
+  export default Header
