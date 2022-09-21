0fff906f1761490c064cc56c7ae5193b489174a7
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-09-21 06:32:08 -0600

    Breadcrumbs added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Header from './components/Header'
 import Main from './components/Main'
 import Navigation from './components/Navbar'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Breadcrumbs from './components/Breadcrumbs'
 
 function App() {
   return (
+    <>
+    
     <div className="App">
       <Header/>
       <Navigation/>
+      <Breadcrumbs/>
       <Main/>
      
     </div>
+    </>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import {React} from 'react'
+
+const About = () =>{
+    return(
+    <div>
+        <img src="https://i.imgur.com/tde9GdW.jpg" alt="Chef Jonelle" class="chef-pic"/>
+        <h1>Chef Jonelle</h1>
+        <p>Chef Jonelle got her start in upstate NY cooking for fine dining restaurants. She then studied culinary arts at Johnson and Wales in Denver,CO. After graduating she worked as a compliance manager for a large fruit, nut, and candy distributor. After a few years, she felt the calling to get back to her roots. Working as a personal chef over the last decade has helped her fine tune her skills while balancing the various needs of her clients. As owner and operator of Colordo Healthy Homemade, she passionaly pursues her craft while ensuring her clients eat well. Please do not hesitate and reach out  for additional information. </p>
+    </div>
+    )
+}
+
+export default About
