47ebed29aa2a64d5b71b06d801fb8622b972f2ef
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-09-21 13:56:49 -0600

    Register and Login created

---
+++ src/pages/Login.js
@@ -0,0 +1,37 @@
+import {React} from 'react'
+import {  useNavigate } from 'react-router-dom'
+import './Pages.css'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/Navbar.css'
+
+
+const BASE_URL = 'https://co-healthy-homemade.herokuapp.com/'
+
+const Login = () =>{
+    const navigate = useNavigate()
+    const URL = BASE_URL+'login'
+    
+    return(
+        <Form id="login-form">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    )
+}
+
+export default Login
---
+++ src/pages/Register.js
@@ -0,0 +1,53 @@
+import {React} from 'react'
+import {  useNavigate } from 'react-router-dom'
+import './Pages.css'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/Navbar.css'
+
+
+const BASE_URL = 'https://co-healthy-homemade.herokuapp.com/'
+
+const Register = () =>{
+    const navigate = useNavigate()
+    const URL = BASE_URL+'register'
+    
+    return(
+        <Form id="register-form">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPhone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control type="number" placeholder="Phone" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicAddress">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="Address" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicAddress">
+          <Form.Label>Address</Form.Label>
+          <Form.Control type="text" placeholder="City, State" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    )
+}
+
+export default Register
