26ba5f043b0e5175b08c20c10d0ea6f0ecb95a66
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-09-21 09:54:01 -0600

    Services page fonts and spacing complete

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,8 +3,6 @@ import { useParams, Link, useNavigate } from 'react-router-dom'
 import './Pages.css'
 
 
-
-
 const BASE_URL = 'https://co-healthy-homemade.herokuapp.com/'
 
 const Services = () =>{
@@ -12,7 +10,7 @@ const Services = () =>{
     const URL = BASE_URL+'services'
     
     return(
-    <div>
+    <div id="services">
         <h1>Services</h1>
 
         <hr></hr>
@@ -20,7 +18,7 @@ const Services = () =>{
         <h2>Custom In-Home Weekly Meal Prep</h2>
 
         <p>Colorado Healthy Homemade is a Denver based in-home personal chef service. Chef Jonelle uses her years of culinary experience to provide healthy, diverse, and diet specific meal plans. Saving her clients time and money while reducing the need for impulse meals that tend to fall short on quality. </p>
-        <p>Our service begins with a chef consultation to detail any allergies, dietary requirements, and personal tastes. On the day of your service, the chef comes to your home and inventories what ingredients you have and constructs a menu. Your preferences and seasonal availability are all considered during the menu creation. The chef then does the shopping for you! This saves you time and ensures that your meals are made with the best ingredients available. The chef then returns to your home and prepares the meals taylred to your required  portions for the week. Once the preparation is complete, you are provided the weekly menu and photo references via Co Healthy Homemade's web-based application. This provides the client a place to view the menu, review  reference photos, plan out the week, and provide feedback directly to the chef. Likes, dislikes, and changes are encouraged and a necessary part of our service. </p>
+        <p>Our service begins with a chef consultation to detail any allergies, dietary requirements, and personal tastes. On the day of your service, the chef comes to your home and inventories what ingredients you have and constructs a menu. Your preferences and seasonal availability are all considered during the menu creation. The chef then does the shopping for you! This saves you time and ensures that your meals are made with the best ingredients available. The chef then returns to your home and prepares the meals tailored to your required portions for the week. Once the preparation is complete, you are provided the weekly menu and photo references via Co Healthy Homemade's web-based application. This provides the client a place to view the menu, review  reference photos, plan out the week, and provide feedback directly to the chef. Likes, dislikes, and changes are encouraged and a necessary part of our service. </p>
         <h3> Rate: $53/hr 3hr minimum Includes: Menu Planning, Shopping, Preparing, and Cleaning</h3>
 
         <hr></hr>
