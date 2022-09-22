a771cf393a16cfe4a23678f670c1cae4e1f4600e
maclamour <maclamour@gmail.com>
2022-09-22 09:23:49 -0400

    working on email works

--- src/pages/Email.js
+++ src/pages/Email.js
@@ -16,14 +16,22 @@ export const Email = () => {
   };
 
   return (
+    <div classname = "container">
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
+    <label for="country">Service Needed</label>
+        <select id="country" name="country">
+            <option value="australia">Meal Order</option>
+            <option value="canada">Canada</option>
+            <option value="usa">USA</option>
+    </select>
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
     </form>
+    </div>
   );
 };
