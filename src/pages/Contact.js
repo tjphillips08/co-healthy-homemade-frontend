625283d24880691f8c1c5e37812ae169de1f7438
maclamour <maclamour@gmail.com>
2022-09-22 13:06:18 -0400

    changed link in contact page to route to email page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ const Contact = () =>{
     <div id="contact-us">
         <h1 id="contact-text">Contact Us</h1>
         <h2>Ph:(720)-951-1183</h2>
-        <h2>Em:cohealthyhomemade@gmail.com</h2>
+        <a href="/email" id="email">Email the Chef!</a>
         <p id="chef-info">For consultation requests: Please send an email including your name, location, portions needed per week, and availability</p>
         <h2>Thank you for visiting! -Chef Jonelle</h2>
     </div>
